1974d0c845ec1ac1f739c653a0e6f0078a246854
Aurelie <aurelie.tacquard@gmail.com>
2025-10-20 15:19:38 +0100

    set up initial states for data fetching

--- src/hooks/useRobinData.tsx
+++ src/hooks/useRobinData.tsx
@@ -1,2 +1,11 @@
-export const useRobinData = () => {
-}
+import { RobinSightings } from "../types";
+import { useState } from "react";
+
+export const useRobinData = (fetchData: number) => {
+  const [weeklyData, setWeeklyData] = useState<RobinSightings[][]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [missingDates, setMissingDates] = useState<Set<string>>(new Set());
+
+  return { weeklyData, loading, error, missingDates };
+};
