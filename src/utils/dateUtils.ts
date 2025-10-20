50c44614d0a7c95c2c69f18c8838878fc064042f
Aurelie <aurelie.tacquard@gmail.com>
2025-10-20 15:07:28 +0100

    create dateUtils.ts file + add parseDate and formatDate functions

---
+++ src/utils/dateUtils.ts
@@ -0,0 +1,13 @@
+// Converts date into a JavaScript Date object
+export const parseDate = (dateStr: string) => {
+  const [day, month, year] = dateStr.split("/").map(Number);
+  return new Date(year, month - 1, day);
+};
+
+// Format date back to DD/MM/YYYY
+export const formatDate = (date: Date) => {
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
