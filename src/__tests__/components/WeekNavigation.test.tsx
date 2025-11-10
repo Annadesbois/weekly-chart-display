5ec793010ae8949a9f2b9fe6721146a55e2bc857
Aurelie <aurelie.tacquard@gmail.com>
2025-11-10 17:15:05 +0000

    improve imports order

--- src/__tests__/components/WeekNavigation.test.tsx
+++ src/__tests__/components/WeekNavigation.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+
 import { WeekNavigation } from "@/components/WeekNavigation";
+import userEvent from "@testing-library/user-event";
 
 const getPrevBtn = () => screen.getByText("⬅ Previous Week");
 const getNextBtn = () => screen.getByText("Next Week ➡");
